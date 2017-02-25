2ae287b71f4ed5937187f34531711acb74620cf5
natanid <natanid@yandex.ua>
2017-02-25 20:24:05 +0200

    Coins

--- LogosMassIf/src/ua/study/lesson05/Main.java
+++ LogosMassIf/src/ua/study/lesson05/Main.java
@@ -36,48 +36,37 @@ public class Main {
 		int _25 = 25;
 		int _50 = 50;
 		int number = 127;
-		int number1 = 0;
-		int number2 = 0;
-		int number3 = 0;
-		int count50 = 0;
-		int count25 = 0;
+		int count = 0;
 		if (number > _50) {
-			number1 = number % _50; // 127%50 = 27
-			count50 = number / _50;
-			System.out.println(number1);
-			System.out.println("50 coins - " +count50);
-			System.out.println("fff \n \r \\ \' \" \t \b \f fff");
+			count = number / _50;
+			System.out.println("50 coins - " + count);
+			number = number % _50; // 127%50 = 27
 		}
-		if (number1 > _25) {
-			number2 = number % _25; // 27%25 = 2
-			count25 = number1 / _25;
-			System.out.println(number2);
-			System.out.println("25 coins - " +count25);
+		if (number > _25) {
+			count = number / _25;
+			System.out.println("25 coins - " + count);
+			number = number % _25; // 27%25 = 2
 		}
-		if (number2 > _10) {
-			number3 = number % _25;
-			System.out.println("\n" +number3);
-			
+		if (number > _10) {
+			count = number / _10;
+			System.out.println("10 coins - " + number);
+			number = number % _10;
 		}
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		if (number > _5) {
+			count = number / _5;
+			System.out.println("5 coins - " + number);
+			number = number % _5;
+		}
+		if (number > _2) {
+			count = number / _2;
+			System.out.println("2 coins - " + number);
+			number = number % _2;
+		}
+		if (number > _1) {
+			count = number / _1;
+			System.out.println("1 coins - " + number);
+			number = number % _1;
+		}
+		// System.out.println("fff \n \r \\ \' \" \t \b \f fff");
 	}
 }
